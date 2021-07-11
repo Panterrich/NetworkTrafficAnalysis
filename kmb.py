f0752664ad20a31e35ac56fc00cc21b6e8590cc2
Panterrich <durnov.an@phystech.edu>
2021-07-11 03:19:55 +0300

    FEATURE: add logging module

--- kmb.py
+++ kmb.py
@@ -6,6 +6,7 @@ to which the server part sends him the address and port of the client in the for
 
 import sys
 import socket
+import logging
 
 
 def client_tcp_interface(host, port):
@@ -18,13 +19,16 @@ def client_tcp_interface(host, port):
     :return:
     """
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP socket
     client_socket.connect((host, port))
+    logging.info('The client TCP socket was opened')
 
     data = client_socket.recv(4096)
+    logging.info('The client received a message from the server %s:%d', host, port)
     print(data.decode())
 
     client_socket.close()
+    logging.info('The client TCP socket was closed')
 
 
 def client_udp_interface(host, port):
@@ -40,11 +44,14 @@ def client_udp_interface(host, port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket
 
     client_socket.sendto(''.encode("utf-8"), (host, port))  # send message to server
+    logging.info('The client sent a request to the server %s:%d', host, port)
 
     data = client_socket.recvfrom(4096)
+    logging.info('The client received a message from the server %s:%d', host, port)
     print(data[0].decode())
 
     client_socket.close()
+    logging.info('The client UDP socket was closed')
 
 
 def server_tcp_interface(address, port):
@@ -60,16 +67,21 @@ def server_tcp_interface(address, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((address, port))
     server_socket.listen(1)
-
-    print("The server is ready to receive")
+    logging.info('The server is ready to receive')
 
     while 1:
         connection_socket, client_address = server_socket.accept()
+        logging.info('The TCP connection to the client %s:%d has been set up',
+                     client_address[0], client_address[1])
 
         answer = (str(client_address[0]) + ':' + str(client_address[1])).encode('utf-8')
 
         connection_socket.send(answer)
+        logging.info('The server sent a message to the client %s:%d',
+                     client_address[0], client_address[1])
+
         connection_socket.close()
+        logging.info('The TCP connection socket was closed')
 
 
 def server_udp_interface(address, port):
@@ -84,16 +96,19 @@ def server_udp_interface(address, port):
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((address, port))
-
-    print("The server is ready to receive")
+    logging.info('The server is ready to receive')
 
     while 1:
         data = server_socket.recvfrom(4096)
         client_address = data[1]
+        logging.info('The server received a message from the client %s:%d',
+                     client_address[0], client_address[1])
 
         answer = (str(client_address[0]) + ':' + str(client_address[1])).encode('utf-8')
 
         server_socket.sendto(answer, client_address)
+        logging.info('The server sent a message to the client %s:%d',
+                     client_address[0], client_address[1])
 
 
 def check_address(str_host, str_port):
@@ -102,23 +117,30 @@ def check_address(str_host, str_port):
 
     :param str_host: a string containing the host name
     :param str_port: a string containing the port number
-    :return:
+    :return: error
     """
 
     if str_port.isdigit():
         if int(str_port) < 0 or int(str_port) > 65536:
-            sys.exit("ERROR, invalid port \n"
-                     "Please, run the program in the following format: \n"
-                     "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
+            logging.error("Invalid port! "
+                          "The port number has exceeded the range of acceptable values \n"
+                          "Please, run the program in the following format: \n"
+                          "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
+            return 0
     else:
-        sys.exit("ERROR, invalid port \n"
-                 "Please, run the program in the following format: \n"
-                 "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
+        logging.error("Invalid port! "
+                      "The port number doesn't consist of digits \n"
+                      "Please, run the program in the following format: \n"
+                      "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
+        return 0
 
     if str_host[0] == '-':
-        sys.exit("ERROR, invalid host name \n"
-                 "Please, run the program in the following format: \n"
-                 "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
+        logging.error("Invalid host name \n"
+                      "Please, run the program in the following format: \n"
+                      "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
+        return 0
+
+    return 1
 
 
 def config_connection(flags):
@@ -126,16 +148,18 @@ def config_connection(flags):
     Analyzes command-line arguments
 
     :param flags: the rest of the command line arguments
-    :return: 0 - client tcp
-             1 - server tcp
-             2 - client udp
-             3 - server udp
+    :return: mask: 0 - client tcp
+                   1 - server tcp
+                   2 - client udp
+                   3 - server udp
+             different_protocols_flag - the flag is set if UDP and TCP are used at the same time.
     """
 
     mask = 0
+    different_protocols_flag = 0
 
     if len(flags) == 0:
-        return mask
+        return mask, different_protocols_flag
 
     if '-s' in flags:
         mask += 1
@@ -144,28 +168,56 @@ def config_connection(flags):
         mask += 2
 
     if '-t' in flags and (mask & 0b10) >> 1:
-        sys.exit("ERROR, invalid flags. The -t and -u flags are not supported at the same time \n"
-                 "Please, run the program in the following format: \n"
-                 "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
-    return mask
+        different_protocols_flag = 1
+        logging.error("Invalid flags. The -t and -u flags are not supported at the same time \n"
+                      "Please, run the program in the following format: \n"
+                      "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
+
+    return mask, different_protocols_flag
+
+
+def log_config():
+    """
+    Analyzes the flags and configures the logging format and the log output stream
+
+    :return: logging is configured successfully or not
+    """
+
+    if '-f' in sys.argv:
+        if '-o' in sys.argv:
+            logging.basicConfig(level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+            logging.error("Multiple output streams are used."
+                          "Please, run the program in the following format: \n"
+                          "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
+            return 0
+
+        logging.basicConfig(filename=sys.argv[sys.argv.index('-f') + 1],
+                            level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+        return 1
+
+    logging.basicConfig(level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+    return 1
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        sys.exit("ERROR, incorrectly entered arguments when starting the program \n"
-                 "Please, run the program in the following format: \n"
-                 "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
-    else:
-        host_name = sys.argv[1]
-        number_port = sys.argv[2]
+    if log_config():
+        if len(sys.argv) < 3:
+            logging.error("Incorrectly entered arguments when starting the program \n"
+                          "Please, run the program in the following format: \n"
+                          "python3 kmb.py <host> <port> [-s] [-t | -u] [-o | -f <file>] \n")
+        else:
+            host_name = sys.argv[1]
+            number_port = sys.argv[2]
 
-        check_address(host_name, number_port)
+            if check_address(host_name, number_port):
 
-        flag = sys.argv[3:]
+                flag = sys.argv[3:]
+                mode, error = config_connection(flag)
 
-        config = {0: client_tcp_interface,
-                  1: server_tcp_interface,
-                  2: client_udp_interface,
-                  3: server_udp_interface}
+                if not error:
+                    config = {0: client_tcp_interface,
+                              1: server_tcp_interface,
+                              2: client_udp_interface,
+                              3: server_udp_interface}
 
-        config.get(config_connection(flag))(host_name, int(number_port))
+                    config.get(mode)(host_name, int(number_port))
